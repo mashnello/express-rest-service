c0a99115c46258873ba22de7a06b8028049142f9
mashnello <>
2021-05-16 16:14:29 +0200

    feat: add Column model

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,10 +1,11 @@
 const { v4: uuid } = require('uuid');
+const Column = require('./column.model');
 
 class Board {
   constructor({ id = uuid(), title, columns = [] } = {}) {
     this.id = id;
     this.title = title;
-    this.columns = columns;
+    this.columns = columns.map((column) => new Column(column));
   }
 
   static toResponse(board) {
