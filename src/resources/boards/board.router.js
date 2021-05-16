f8aa91a1b165df854e9a7ed5c0976e1585dad048
mashnello <>
2021-05-16 16:37:46 +0200

    refactor: routers

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -2,59 +2,58 @@ const router = require('express').Router();
 const Board = require('./board.model');
 const boardsService = require('./board.service');
 
-router
-  .route('/')
-  .get(async (req, res) => {
-    const boards = await boardsService.getAll();
-    res.json(boards.map(Board.toResponse));
-  })
-  .post(async (req, res) => {
-    try {
-      const { id, title, columns } = req.body;
-      const board = await boardsService.create({
-        id,
-        title,
-        columns,
-      });
-      res.status(201).json(Board.toResponse(board));
-    } catch (error) {
-      res.status(400).end();
-    }
-  });
+router.get('/', async (req, res) => {
+  const boards = await boardsService.getAll();
+  res.json(boards.map(Board.toResponse));
+});
 
-router
-  .route('/:id')
-  .get(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const board = await boardsService.getById(id);
-      res.json(Board.toResponse(board));
-    } catch (error) {
-      res.status(404).end();
-    }
-  })
-  .put(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { title, columns } = req.body;
-      const board = await boardsService.update({
-        id,
-        title,
-        columns,
-      });
-      res.json(Board.toResponse(board));
-    } catch (error) {
-      res.status(400).end();
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const board = await boardsService.deleteById(id);
-      res.status(204).json(Board.toResponse(board));
-    } catch (error) {
-      res.status(404).end();
-    }
-  });
+router.post('/', async (req, res) => {
+  try {
+    const { id, title, columns } = req.body;
+    const board = await boardsService.create({
+      id,
+      title,
+      columns,
+    });
+    res.status(201).json(Board.toResponse(board));
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const board = await boardsService.getById(id);
+    res.json(Board.toResponse(board));
+  } catch (error) {
+    res.status(404).end();
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, columns } = req.body;
+    const board = await boardsService.update({
+      id,
+      title,
+      columns,
+    });
+    res.json(Board.toResponse(board));
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const board = await boardsService.deleteById(id);
+    res.status(204).json(Board.toResponse(board));
+  } catch (error) {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,51 +2,50 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router
-  .route('/')
-  .get(async (req, res) => {
-    const users = await usersService.getAll();
-    res.json(users.map(User.toResponse));
-  })
-  .post(async (req, res) => {
-    try {
-      const { name, login, password } = req.body;
-      const user = await usersService.create({ name, login, password });
-      res.status(201).json(User.toResponse(user));
-    } catch (error) {
-      res.status(400).end();
-    }
-  });
+router.get('/', async (req, res) => {
+  const users = await usersService.getAll();
+  res.json(users.map(User.toResponse));
+});
 
-router
-  .route('/:id')
-  .get(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await usersService.getById(id);
-      res.json(User.toResponse(user));
-    } catch (error) {
-      res.status(404).end();
-    }
-  })
-  .put(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { name, login, password } = req.body;
-      const user = await usersService.update({ id, name, login, password });
-      res.json(User.toResponse(user));
-    } catch (error) {
-      res.status(400).end();
-    }
-  })
-  .delete(async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await usersService.deleteById(id);
-      res.status(204).json(user);
-    } catch (error) {
-      res.status(404).end();
-    }
-  });
+router.post('/', async (req, res) => {
+  try {
+    const { name, login, password } = req.body;
+    const user = await usersService.create({ name, login, password });
+    res.status(201).json(User.toResponse(user));
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await usersService.getById(id);
+    res.json(User.toResponse(user));
+  } catch (error) {
+    res.status(404).end();
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, login, password } = req.body;
+    const user = await usersService.update({ id, name, login, password });
+    res.json(User.toResponse(user));
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await usersService.deleteById(id);
+    res.status(204).json(user);
+  } catch (error) {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
