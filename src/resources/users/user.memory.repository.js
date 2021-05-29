1f888f941acb4f3125493dd5bfb3b071b3d3f30a
mashnello <7679625+mashnello@users.noreply.github.com>
2021-05-29 21:54:50 +0200

    Express REST service (#1)

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,33 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+const { validateItemExists } = require('../../helpers/validation');
+
+let USERS = [];
+
+const getAll = async () => USERS;
+
+const getById = async (id) => {
+  validateItemExists(id, USERS);
+  return USERS.find((item) => item.id === id);
 };
 
-module.exports = { getAll };
+const create = async (user) => {
+  const aUser = new User(user);
+  USERS.push(aUser);
+  return aUser;
+};
+
+const update = async (user) => {
+  validateItemExists(user.id, USERS);
+  USERS = USERS.map((item) =>
+    item.id === user.id ? { ...item, ...user } : item
+  );
+  return USERS.find((item) => item.id === user.id);
+};
+
+const deleteById = async (id) => {
+  validateItemExists(id, USERS);
+  USERS = USERS.filter((item) => item.id !== id);
+  return {};
+};
+
+module.exports = { getAll, getById, create, update, deleteById };
