3439a72077ccf469d9dfa4c364bb1785129293b1
mashnello <>
2021-05-29 22:09:48 +0200

    Merge branch 'master' of https://github.com/mashnello/express-rest-service into task4/typescript

--- src/helpers/validation.js
+++
@@ -1,8 +0,0 @@
-const validateItemExists = (id, list) => {
-  const isExisting = list.find((item) => item.id === id);
-  if (!isExisting) {
-    throw new Error('Not found');
-  }
-};
-
-module.exports = { validateItemExists };
